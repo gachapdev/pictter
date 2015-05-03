938610164c9367f0db32453bb016be87f7230017
elzzup <guild0105@gmail.com>
2015-05-04 00:09:20 +0900

    update input method on enter

--- app/src/main/java/com/elzup/pictter/pictter/MainActivity.java
+++ app/src/main/java/com/elzup/pictter/pictter/MainActivity.java
@@ -1,19 +1,15 @@
 package com.elzup.pictter.pictter;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,14 +18,11 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.wdullaer.swipeactionadapter.SwipeActionAdapter;
 import com.wdullaer.swipeactionadapter.SwipeDirections;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -40,7 +33,7 @@ public class MainActivity extends ActionBarActivity
     private PictureStatusAdapter pictureStatusAdapter;
     private TwitterManager twitterManager;
 
-    private EditText search_box;
+    private EditText searchEditText;
     private SwipeActionAdapter swipeAdapter;
 
     private NavigationDrawerFragment mNavigationDrawerFragment;
@@ -159,22 +152,36 @@ public class MainActivity extends ActionBarActivity
     }
 
     private void setupSearchForm() {
-        this.search_box = (EditText) findViewById(R.id.searchBar);
-        search_box.setFocusable(true);
-        Button searchButton = (Button) findViewById(R.id.searchButton);
+        searchEditText = (EditText) findViewById(R.id.searchBar);
+        searchEditText.setFocusable(true);
+        final Button searchButton = (Button) findViewById(R.id.searchButton);
         searchButton.setOnClickListener(new onClickSearchListener());
         inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        searchEditText.setOnKeyListener(new View.OnKeyListener() {
+
+            //コールバックとしてonKey()メソッドを定義
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    searchButton.callOnClick();
+                    return true;
+                }
+
+                return false;
+            }
+        });
     }
 
     class onClickSearchListener implements View.OnClickListener {
 
         @Override
         public void onClick(View v) {
-            String keyword = search_box.getText().toString();
+            String keyword = searchEditText.getText().toString();
             if ("".equals(keyword)) {
                 return;
             }
             mTitle = "Pictter - " + keyword;
+            getSupportActionBar().setTitle(mTitle);
             pictureStatusAdapter.clear();
             twitterManager.searchTweets(keyword, null, getResources().getInteger(R.integer.search_tweet_limit), pictureStatusAdapter);
             killFocus();
